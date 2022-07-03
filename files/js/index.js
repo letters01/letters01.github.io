6f21fb11235ec91ce21ff850cda212a3300d19e4
letters01 <82847345+letters01@users.noreply.github.com>
2022-07-02 20:37:35 -0500

    Update index.js

--- files/js/index.js
+++ files/js/index.js
@@ -1,22 +1,27 @@
+
 var buttons = document.querySelectorAll('.list-group .container-buttons button');
 buttons.forEach(butt => {
+    /*
     butt.addEventListener('click', () => {
         var idMod = butt.getAttribute('id');
         switch (idMod) {
             case 'TMO':
                 copyClipboard('MangasDotNet.json');
+                
                 break;
         
             default:
                 break;
         }
     });
-    butt.addEventListener('pointerdown', () => {
+    */
+    butt.addEventListener('pointerdown', async () => {
         document.getElementById('prueba').innerText = 'PointerDown';
         var idMod = butt.getAttribute('id');
         switch (idMod) {
             case 'TMO':
-                copyClipboard('MangasDotNet.json');
+                await copyClipboard('MangasDotNet.json');
+                
                 break;
         
             default:
@@ -25,18 +30,24 @@ buttons.forEach(butt => {
     });
 });
 
-
-
 async function copyClipboard(modulo) {
     try {
-        await navigator.clipboard.writeText(location.href);
+        //await navigator.clipboard.writeText(location.href);
         document.getElementById('result').innerText = 'Copiado';
     } catch (err) {
         document.getElementById('result').innerText = 'Error: ' + err;
     }
-    try{
-        copy(location.href);
-        document.getElementById('result2').innerText = 'Copiado';
+    try {
+        await fetch('modules/MangasDotNet.json')
+        .then(resp => resp.text())
+        .then(text => {
+            navigator.clipboard.writeText(text);
+        })
+        .finally(()=> {
+            document.getElementById('result2').innerText = 'Copiado';
+        });
+        
+       
     } catch (err) {
         document.getElementById('result2').innerText = 'Error: ' + err;
     }
